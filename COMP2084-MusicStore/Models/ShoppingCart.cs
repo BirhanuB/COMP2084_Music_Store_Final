19d870bd9eb54bf00887d5ef5289de441cfbecc6
Birhanu Bishaw <200414083@student.georgianc.on.ca>
2019-11-11 18:49:04 -0500

    Added shpping cart

---
+++ COMP2084-MusicStore/Models/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId { get; set; }
+
+        public ICollection<ShoppingCartLineItem> ShoppingCartLineItems { get; set; }
+
+        public  void AddToCart(MusicStoreContext db, Song song)
+        {
+
+        }
+
+        public static ShoppingCart GetUserCart(HttpContext http)
+        {
+            var cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(http);
+            return cart;
+        }
+
+        public string GetCartId(HttpContext http)
+        {
+            if (http.Session.GetString("CartId") == null)
+            {
+                if (!string.IsNullOrWhiteSpace(http.User.Identity.Name))
+                {
+                    http.Session.SetString("CartId", http.User.Identity.Name);
+                }
+            }
+
+            return http.Session.GetString("CartId");
+        }
+    }
+}
---
+++ COMP2084-MusicStore/Models/ShoppingCartLineItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class ShoppingCartLineItem
+    {
+        public int ShoppingCartLineItemId { get; set; }
+
+        [ForeignKey("ShoppingCart")]
+        public string ShoppingCartId { get; set; }
+
+        [ForeignKey("Song")]
+        public int SongId { get; set; }
+
+        public int Count { get; set; }
+    }
+}
